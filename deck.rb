3cc378177684df30c7eac64856f094fcd3f17c33
Sergey <asa@angelita.ru>
2017-07-12 16:48:29 +0700

    Fixed start from 2

--- deck.rb
+++ deck.rb
@@ -6,7 +6,7 @@ class Deck
   def initialize
     @cards = []
     ["\u2660", "\u2665", "\u2663", "\u2666"].each do |suit|
-      [(6..10).to_a, 'J', 'Q', 'K', 'A'].flatten.each do |name|
+      [(2..10).to_a, 'J', 'Q', 'K', 'A'].flatten.each do |name|
         @cards << Card.new(name, suit)
       end
     end
